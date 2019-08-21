89727cdb6d0b0f1620cd21f35e2fafa0dce555a6
joaoandrade02 <54117022+joaoandrade02@users.noreply.github.com>
2019-08-21 14:31:00 -0300

    Delete filter.js

--- filter.js
+++
@@ -1,4 +0,0 @@
-const woman = require("./data.js");
-const allwoman = woman.map(woman => woman.gender.toLocaleLowerCase() = "female" );
-const filteredwoman = allwoman.filter(allwoman => allwoman.age >=18);
-console.table(filteredwoman);
